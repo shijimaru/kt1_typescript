d5de6d47dde6edb45f37bfc0aceb87771803108f
shijimaru <124866373+shijimaru@users.noreply.github.com>
2024-03-12 20:47:39 +1000

    app.ts

---
+++ app.ts
@@ -0,0 +1,68 @@
+class User{ 
+    name: string; 
+    readonly login: string; 
+    protected password: string; 
+    private grade: number; 
+    static count: number = 0; 
+ 
+    constructor(name: string, login: string, password: string, grade: number){ 
+         this.name = name; 
+         this.login = login; 
+         this.password = password; 
+         this.grade = grade; 
+         User.count++ 
+    } 
+ 
+    set Login(login:string){ 
+        console.log("Невозможно изменить логин!") 
+    } 
+ 
+    set Password(password:string){ 
+        console.log("***********") 
+    }  
+     
+    set Grade(grade:string){ 
+        console.log("Неизвестно свойство grade!") 
+    } 
+ 
+    static show_info(user: User) { 
+        console.log(user.name, user.login) 
+    } 
+
+    eq(user: User) { 
+        return this.login === user.login 
+    }
+
+    lt(user: User) { 
+        return this.login > user.login 
+    }
+
+    gt(user: User) { 
+        return this.login < user.login 
+    }
+} 
+ 
+class SuperUser extends User{ 
+    name: string; 
+    login: string; 
+    password: string; 
+    role: string; 
+    static count: number; 
+ 
+    constructor(_name: string, _login: string, _password: string, _grade: number, _role: string){ 
+        super(_name, _login, _password,_grade) 
+        this.name = _name; 
+        this.login = _login; 
+        this.password = _password; 
+        this.role = _role; 
+        User.count++ 
+    } 
+
+    set Role(role:string){ 
+        console.log(role) 
+    } 
+ 
+    static show_info(superuser: SuperUser) { 
+        console.log(superuser.name, superuser.login, superuser.role) 
+    } 
+}
